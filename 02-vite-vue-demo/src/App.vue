a22ffe51caa0ee2a5f6793389f8d96e5feb6b488
yuliang <2917789402@qq.com>
2026-09-07 20:05:13 +0800

    feat:练习 vue 基础指令语法

--- 02-vite-vue-demo/src/App.vue
+++ 02-vite-vue-demo/src/App.vue
@@ -1,10 +1,36 @@
 <template>
-  <h2>Vite创建Vue项目</h2>
-  <p>{{ message }}</p>
+  <div>
+  
+    <p>姓名：{{ name }}</p>
+
+    <img v-bind:Src="imgUrl" alt="">
+
+    <button @click="changeName">修改名字</button>
+
+    <input v-model="name"/>
+
+    <p v-if="isShow">显示内容</p>
+
+    <ul>
+      <li v-for="item in list" :key="item.id">{{ item.title }}
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script setup>
-let message = "学习Vue基础语法"
+import { ref } from 'vue'
+const name = ref("张三")
+const imgUrl = ref("https://picsum.photos/100")
+const isShow = ref(true)
+const list = ref([
+  {id:1, title:"Vue学习1"},
+  {id:1, title:"Vue学习2"}
+])
+
+const changeName = ()=>{
+  name.value = "李四"
+}
 </script>
 
 <style>
